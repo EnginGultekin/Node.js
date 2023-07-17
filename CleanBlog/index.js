fb6633dc273483949cb80254271a43f9a593a572
Engin Gültekin <engingultekin1071@gmail.com>
2023-07-17 08:29:15 +0300

    Express and nodemon package installed and index.js started.

--- CleanBlog/index.js
+++ CleanBlog/index.js
@@ -1,5 +1,17 @@
-const fn = function () {
-    console.log('Function Working');
-}
+import express from "express";
 
-fn();
+const app = express();
+
+app.get('/', (req, res) => {
+    const blog = {
+        id: 1,
+        title: "Blog title",
+        description: "Blog description",
+    }
+    res.send(blog);
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}...`);
+})
