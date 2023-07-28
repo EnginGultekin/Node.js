128fbd4d1a5885e147a84be25888d9043f9320f3
Engin Gültekin <engingultekin1071@gmail.com>
2023-07-28 10:53:55 +0300

    Static files uploaded, dynamic files uploaded, and edited with ejs package

--- SmartEdu/index.js
+++ SmartEdu/index.js
@@ -2,11 +2,25 @@ import express from 'express';
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.status(200).send('Îndex Page');
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server started on port ${port}...`);
+    console.log(`Server started on port ${port}...`);
 });
