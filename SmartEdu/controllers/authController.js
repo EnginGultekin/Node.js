2068551abd3123ce4ee402db8cf843d5e29efdec
Engin Gültekin <engingultekin1071@gmail.com>
2023-08-01 17:01:44 +0300

    Dashboard page edited

--- SmartEdu/controllers/authController.js
+++ SmartEdu/controllers/authController.js
@@ -27,7 +27,7 @@ const loginUser = asynchandler(async (req, res) => {
                 if (same) {
                     // User Session
                     req.session.userID = user._id;
-                    res.status(200).redirect('/');
+                    res.status(200).redirect('/users/dashboard');
                 }
             });
         }
@@ -46,8 +46,19 @@ const logoutUser = (req, res) => {
     });
 };
 
+// Kullanıcı giriş yaptıktan sonra görünen bir sayfa ve 
+// kullanıcı rollerine göre şekilleneceği için buırda tanımlı
+const getDashboardPage = asynchandler(async (req, res) => {
+    const user = await User.findOne({ _id: req.session.userID })
+    res.status(200).render('dashboard', {
+        page_name: 'dashboard',
+        user,
+    });
+});
+
 export default {
     createUser,
     loginUser,
     logoutUser,
+    getDashboardPage,
 };
